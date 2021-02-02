1ffe4df4de8bada86f9a36541663a105278d1cda
ritik0701 <gritik1997@gmail.com>
2021-02-02 16:28:19 +0530

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { OpenAccountComponent } from './open-account/open-account.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { AddBeneficiaryComponent } from './add-beneficiary/add-beneficiary.component';
+import { AccountStatusComponent } from './account-status/account-status.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    LogoutComponent,
+    OpenAccountComponent,
+    TransactionComponent,
+    AddBeneficiaryComponent,
+    AccountStatusComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
